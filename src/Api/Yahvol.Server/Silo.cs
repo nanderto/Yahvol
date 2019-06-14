90eac2c31c472faf0834ca14e364d954ff662e32
Noel <noaleanderton@live.com>
2019-06-14 16:29:25 -0700

    Added new web server with Orleans

---
+++ src/Api/Yahvol.Server/Silo.cs
@@ -0,0 +1,12 @@
+﻿using Orleans.Runtime;
+using System;
+
+namespace Yahvol.Server
+{
+    public class Silo : Orleans.Runtime.Silo
+    {
+        public Silo(ILocalSiloDetails siloDetails, IServiceProvider services) : base(siloDetails, services)
+        {
+        }
+    }
+}
---
+++ src/Api/Yahvol.Web/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Hosting;
+
+namespace Yahvol.Web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var silo = new SiloHostBuilder()
+                .UseLocalhostClustering()
+                .Build();
+
+            await silo.StartAsync();
+
+            var client = silo.Services.GetRequiredService<IClusterClient>();
+
+            var webHost = new WebHostBuilder()
+                .ConfigureServices(services =>
+                    services
+                        .AddSingleton<IGrainFactory>(client)
+                        .AddSingleton<IClusterClient>(client))
+                .UseStartup<Startup>()
+                // Other ASP.NET configuration...
+                .Build().RunAsync();
+        }
+    }
+}
